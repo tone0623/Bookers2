d891174c547976599771e594475e1fd378fd586b
tone0623 <tone0623@icloud.com>
2021-02-26 11:55:11 +0000

    [Add]Book model

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,6 @@
+class BooksController < ApplicationController
+  
+  def new
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   attachment :profile_image 
+  
+  has_many :books, dependent: :destroy
 end
